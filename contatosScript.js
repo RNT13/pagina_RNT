08c043f13f90f3a7db210e19dd3e18a549e963ce
RNT <renatornt13@gmail.com>
2024-08-31 17:16:33 -0300

    add: arquivos js

--- contatosScript.js
+++ contatosScript.js
@@ -85,3 +85,20 @@ $(document).ready(function () {
         placeholder: 'Telefone Do Contato'
     })
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modalExemplo = new bootstrap.Modal('#exemplo-modal')
+    setTimeout(function(){
+        modalExemplo.show();
+    }, 1000);
+
+    const ativadorToast = document.getElementById('btn-checar-emails')
+        const mensagem = document.getElementById('mensagens-alerta')
+
+        if(ativadorToast){
+            ativadorToast.addEventListener('click', function(){
+                const toast = new bootstrap.Toast(mensagem)
+                toast.show();
+            })
+        }
+})
--- faleConoscoScript.js
+++ faleConoscoScript.js
@@ -37,3 +37,20 @@ $(document).ready(function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modalExemplo = new bootstrap.Modal('#exemplo-modal')
+    setTimeout(function(){
+        modalExemplo.show();
+    }, 1000);
+
+    const ativadorToast = document.getElementById('btn-checar-emails')
+        const mensagem = document.getElementById('mensagens-alerta')
+
+        if(ativadorToast){
+            ativadorToast.addEventListener('click', function(){
+                const toast = new bootstrap.Toast(mensagem)
+                toast.show();
+            })
+        }
+})
