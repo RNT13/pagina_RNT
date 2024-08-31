8e6b9f1195fa8065328a9d9c92b41175a5d446df
RNT <renatornt13@gmail.com>
2024-08-31 18:07:20 -0300

    fix: corrigindo os popups

--- cadastroScript.js
+++ cadastroScript.js
@@ -51,20 +51,3 @@ $(document).ready(function() {
         }
     });
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const modalExemplo = new bootstrap.Modal('#exemplo-modal')
-    setTimeout(function(){
-        modalExemplo.show();
-    }, 1000);
-
-    const ativadorToast = document.getElementById('btn-checar-emails')
-        const mensagem = document.getElementById('mensagens-alerta')
-
-        if(ativadorToast){
-            ativadorToast.addEventListener('click', function(){
-                const toast = new bootstrap.Toast(mensagem)
-                toast.show();
-            })
-        }
-})
